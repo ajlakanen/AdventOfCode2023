c066647b1b7ddef87adb6c2672f9b5bdeeb36e8c
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-01 23:21:02 +0200

    Day 1

--- 01.js
+++ 01.js
@@ -1,9 +1,5 @@
 const f = require("fs");
 
-const sortNumbers = (a, b) => {
-  return parseInt(a) - parseInt(b);
-};
-
 const part1 = () => {
   let lines = [];
 
@@ -23,56 +19,68 @@ const part1 = () => {
     numbers.push(num);
   });
 
-  // console.log(numbers);
-
   const sum = numbers.reduce((a, b) => a + b, 0);
 
   console.log(sum);
 };
 
 const part2 = () => {
-  // 1. read data to lines
   let lines = [];
 
-  const data = f.readFileSync("01-data-part2-small.txt", "utf-8");
+  const data = f.readFileSync("01-data.txt", "utf-8");
   data.split(/\r?\n/).forEach((line) => {
     lines.push(line);
   });
 
-  console.log(lines);
-
-  const numberWords = [
-    { w: "one", n: 1 },
-    { w: "two", n: 2 },
-    { w: "three", n: 3 },
-    { w: "four", n: 4 },
-    { w: "five", n: 5 },
-    { w: "six", n: 6 },
-    { w: "seven", n: 7 },
-    { w: "eight", n: 8 },
-    { w: "nine", n: 9 },
+  const digitWords = [
+    { word: "one", digit: 1 },
+    { word: "two", digit: 2 },
+    { word: "three", digit: 3 },
+    { word: "four", digit: 4 },
+    { word: "five", digit: 5 },
+    { word: "six", digit: 6 },
+    { word: "seven", digit: 7 },
+    { word: "eight", digit: 8 },
+    { word: "nine", digit: 9 },
   ];
 
-  const wordsToNumbers = lines.map((line) => {
-    numberWords.map((number) => {
-      if (line.includes(number.w)) {
-        console.log(line);
-        console.log(number.n);
-        return number.n;
+  /**
+   * Replaces words with digits in a string, while keeping the first and last letter of the word.
+   * For example, "one" becomes "o1e", "three" becomes "t3e", etc.
+   * Special case (example): eighthree -> e8three -> e8t3e
+   * @param {String} str      The string to replace digits in
+   * @param {Array}  mapping  The mapping of words to digits
+   * @returns
+   */
+  const replaceWordDigits = (str, mapping) => {
+    let result = str;
+    let i = 0;
+    while (i < result.length - 1) {
+      for (let j = 0; j < mapping.length; ) {
+        if (i + mapping[j].word.length > result.length) {
+          j++;
+          continue;
+        }
+        const sub = result.substring(i, i + mapping[j].word.length);
+        if (sub === mapping[j].word) {
+          result = result.replace(
+            mapping[j].word,
+            sub[0] + mapping[j].digit + sub[sub.length - 1] // This was a tough one. Thanks to https://www.reddit.com/r/adventofcode/comments/1884fpl/2023_day_1for_those_who_stuck_on_part_2/ for helping me with this.
+          );
+          i = 0;
+          j = 0;
+        } else j++;
       }
-    });
+      i++;
+    }
+    return result;
+  };
+
+  const replaced = lines.map((line) => {
+    return replaceWordDigits(line, digitWords);
   });
 
-  console.log(wordsToNumbers);
-
-  const onlynumbers = lines.map((line) => {
-    line.map((word) => {
-      numberWords.map((number) => {
-        if (word === number.w) {
-          return number.n;
-        }
-      });
-    });
+  const onlynumbers = replaced.map((line) => {
     return line.replace(/\D/g, "");
   });
 
@@ -82,6 +90,16 @@ const part2 = () => {
     const num = parseInt(number[0] + "" + number[number.length - 1]);
     numbers.push(num);
   });
+
+  /* for debugging
+  for (let i = 0; i < lines.length; i++) {
+    console.log(lines[i], replaced[i], onlynumbers[i], numbers[i]);
+  }
+  */
+
+  const sum = numbers.reduce((a, b) => a + b, 0);
+
+  console.log(sum);
 };
 part1();
 part2();
