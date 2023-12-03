e27543bcc7c1c1e1f2294c296a10b7a68d608114
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-03 12:01:15 +0200

    Day 3 part 2

--- 03.js
+++ 03.js
@@ -110,10 +110,109 @@ const part1 = () => {
 const part2 = () => {
   let lines = [];
 
-  const data = f.readFileSync("data/03-data-small.txt", "utf-8");
+  const data = f.readFileSync("data/03-data.txt", "utf-8");
   data.split(/\r?\n/).forEach((line) => {
     lines.push(line);
   });
+
+  // Read all the digits from the lines to a object array
+  // Structure:
+  // [ { digit, i, j },
+  //   { digit, i, j+1 },
+  //   { digit, i, j+2 }, ...
+  // ]
+  let numbers = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    let number = [];
+    for (let j = 0; j < line.length; j++) {
+      const char = line[j];
+      if (char.match(/\d/g)) {
+        number.push({ digit: char, y: i, x: j });
+        if (j === line.length - 1) {
+          numbers.push(number);
+          number = [];
+        }
+      } else {
+        if (number.length > 0) {
+          numbers.push(number);
+          number = [];
+        }
+      }
+    }
+  }
+
+  // Find the locations of the stars
+  let stars = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    for (let j = 0; j < line.length; j++) {
+      const char = line[j];
+      if (char.match(/[*]/g)) {
+        stars.push({ y: i, x: j });
+      }
+    }
+  }
+
+  const directions = [
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+    { x: 0, y: -1 },
+    { x: -1, y: 1 },
+    { x: 1, y: 1 },
+    { x: -1, y: -1 },
+    { x: 1, y: -1 },
+  ];
+
+  // Select all numbers around the stars.
+  // Later, we will select the numbers that appear exactly 2 times
+  let numbersAroundStar = [];
+  for (let i = 0; i < stars.length; i++) {
+    const star = stars[i];
+    const y_star = star.y;
+    const x_star = star.x;
+    for (let j = 0; j < directions.length; j++) {
+      const direction = directions[j];
+      const i_y_new = y_star + direction.y;
+      const i_x_new = x_star + direction.x;
+      // If numbers-array contains a number at the
+      // location of the star, add it to numbersAroundStar
+      const aroundStar = numbers.map((number) =>
+        number.filter((digit) => {
+          if (digit.y === i_y_new && digit.x === i_x_new) {
+            // If number is already added to numbersAroundStar, don't add it again
+            if (!numbersAroundStar.find((n) => n.number === number)) {
+              numbersAroundStar.push({ number: number, star: star });
+            }
+          }
+        })
+      );
+    }
+  }
+
+  // Select stars that appear exactly 2 times in numbersAroundStar
+  let starsWithTwoNumbers = [];
+  for (let i = 0; i < numbersAroundStar.length; i++) {
+    const numberAroundStar = numbersAroundStar[i];
+    const star = numberAroundStar.star;
+    const number = numberAroundStar.number;
+    const count = numbersAroundStar.filter((n) => n.star === star).length;
+    if (count === 2) {
+      starsWithTwoNumbers.push({
+        star: star,
+        number: parseInt(number.map((n) => n.digit).join("")),
+      });
+    }
+  }
+
+  // Multiply the numbers around the stars and add them together
+  let sum = 0;
+  for (let i = 0; i < starsWithTwoNumbers.length; i += 2)
+    sum += starsWithTwoNumbers[i].number * starsWithTwoNumbers[i + 1].number;
+
+  // Print the sum
+  console.log("Part 2", sum);
 };
 
 part1();
