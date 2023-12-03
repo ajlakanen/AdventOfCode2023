891d60e4eba35189f543fda3ac6339b11f601a83
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-03 19:18:43 +0200

    Tidy up and documents

--- 03.js
+++ 03.js
@@ -1,4 +1,36 @@
 const f = require("fs");
+const { get } = require("http");
+
+/**
+ * Read the digits from the lines to a object array
+ * @param {Array} lines Lines of the input
+ * @returns {Array} Digits in the input
+ * @example
+ * const lines = [ '123.456' ];
+ * getNumbers(lines);
+ * // returns [ { digit: '1', y: 0, x: 0 },
+ * //           { digit: '2', y: 0, x: 1 },
+ * //           { digit: '3', y: 0, x: 2 },
+ * //           { digit: '4', y: 0, x: 4 },
+ * //           { digit: '5', y: 0, x: 5 },
+ * //           { digit: '6', y: 0, x: 6 } ]
+ **/
+const getNumbers = (lines) => {
+  let numbers = [];
+  lines.forEach((line, y) => {
+    let number = [];
+    line.split("").forEach((ch, x) => {
+      if (/\d/.test(ch)) {
+        number.push({ digit: ch, y: y, x: x });
+      } else if (number.length) {
+        numbers.push(number);
+        number = [];
+      }
+    });
+    if (number.length) numbers.push(number);
+  });
+  return numbers;
+};
 
 const part1 = () => {
   let lines = [];
@@ -8,44 +40,24 @@ const part1 = () => {
     lines.push(line);
   });
 
-  // Find symbols
-  let symbols = [];
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    for (let j = 0; j < line.length; j++) {
-      const char = line[j];
-      if (!char.match(/[\d\.]/g)) {
-        symbols.push(char);
+  /**
+   * Get all symbols in the input
+   * @param {Array} lines Lines of the input
+   * @returns {Array} Symbols in the input
+   */
+  const getSymbols = (lines) => {
+    let symbols = [];
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      for (let j = 0; j < line.length; j++) {
+        const char = line[j];
+        if (!char.match(/[\d\.]/g) && !symbols.includes(char)) {
+          symbols.push(char);
+        }
       }
     }
-  }
-
-  // Remove duplicate symbols
-  symbols = [...new Set(symbols)];
-  const regex = /[@&*\$\-#=%+/]/g;
-
-  // Read the digits from the lines to a object array
-  // Structure:
-  // [ { digit, i_1, j_1 },
-  //   { digit, i_1, j_1+1 },
-  //   { digit, i_1, j_1+2 }, ...
-  // ],
-  // [ { digit, i_2, j_2 },
-  //   { digit, i_2, j_2+1 }, ...
-  // ], ...
-  let numbers = [];
-  lines.forEach((line, i_y) => {
-    let number = [];
-    line.split("").forEach((ch, i_x) => {
-      if (/\d/.test(ch)) {
-        number.push({ number: ch, i_y, i_x });
-      } else if (number.length) {
-        numbers.push(number);
-        number = [];
-      }
-    });
-    if (number.length) numbers.push(number);
-  });
+    return symbols;
+  };
 
   const directions = [
     { x: -1, y: 0 },
@@ -58,35 +70,46 @@ const part1 = () => {
     { x: 1, y: -1 },
   ];
 
+  /**
+   * Check if there are symbols in any direction of the number
+   * @param {Array}   digit       Digit
+   * @param {Array}   directions  Directions to check
+   * @param {String}  symbols     Symbols to check
+   * @returns {Boolean}           True if there is a symbol in any direction of the number
+   */
+  const symbolInPerimeter = (digit, directions, symbols) => {
+    for (let k = 0; k < directions.length; k++) {
+      const y_new = digit.y + directions[k].y;
+      const x_new = digit.x + directions[k].x;
+      if (
+        lines[y_new] !== undefined &&
+        lines[y_new][x_new] !== undefined &&
+        symbols.includes(lines[y_new][x_new])
+      ) {
+        symbolFound = true;
+        return true;
+      }
+    }
+    return false;
+  };
+
+  // Find symbols
+  const symbols = getSymbols(lines);
+  const numbers = getNumbers(lines);
+
   // Check if there are symbols in any direction of the number
   // If there are, add the number to the sum
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
-
     // Check all digits of the number
     for (let j = 0; j < number.length; j++) {
-      let symbolFound = false;
       const digit = number[j];
-
-      // Check all directions
-      for (let k = 0; k < directions.length; k++) {
-        const i_y_new = digit.i_y + directions[k].y;
-        const i_x_new = digit.i_x + directions[k].x;
-        if (
-          lines[i_y_new] !== undefined &&
-          lines[i_y_new][i_x_new] !== undefined &&
-          lines[i_y_new][i_x_new].match(regex)
-        ) {
-          symbolFound = true;
-          break;
-        }
-      }
-      if (symbolFound) {
+      if (symbolInPerimeter(digit, directions, symbols)) {
         sum += parseInt(
           number
             .map((digit) => {
-              return digit.number;
+              return digit.digit;
             })
             .join("")
         );
@@ -106,35 +129,7 @@ const part2 = () => {
     lines.push(line);
   });
 
-  // Read all the digits from the lines to a object array
-  // Structure:
-  // [ { digit, i_1, j_1 },
-  //   { digit, i_1, j_1+1 },
-  //   { digit, i_1, j_1+2 }, ...
-  // ],
-  // [ { digit, i_2, j_2 },
-  //   { digit, i_2, j_2+1 }, ...
-  // ], ...
-  let numbers = [];
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    let number = [];
-    for (let j = 0; j < line.length; j++) {
-      const char = line[j];
-      if (char.match(/\d/g)) {
-        number.push({ digit: char, y: i, x: j });
-        if (j === line.length - 1) {
-          numbers.push(number);
-          number = [];
-        }
-      } else {
-        if (number.length > 0) {
-          numbers.push(number);
-          number = [];
-        }
-      }
-    }
-  }
+  const numbers = getNumbers(lines);
 
   // Find the locations of the stars
   let stars = [];
