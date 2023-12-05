060493906413e36f8a40fbbded19756920c72bea
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-05 11:12:00 +0200

    day 5 part 2 wip

--- 05.js
+++ 05.js
@@ -1,5 +1,5 @@
 const f = require("fs");
-const { parse } = require("path");
+
 const part1 = () => {
   let lines = [];
 
@@ -91,6 +91,103 @@ const part2 = () => {
   data.split(/\r?\n/).forEach((line) => {
     lines.push(line);
   });
+
+  const groupBy = (xs, key) => {
+    let result = [];
+    let current = [];
+    for (let i = 0; i < xs.length; i++) {
+      const line = xs[i];
+      if (line === key || i === xs.length - 1) {
+        result.push(current);
+        current = [];
+      } else {
+        current.push(line);
+      }
+    }
+
+    return result;
+  };
+
+  const mapNames = [
+    { source: "seed", dest: "soil" },
+    { source: "soil", dest: "fertilizer" },
+    { source: "fertilizer", dest: "water" },
+    { source: "water", dest: "light" },
+    { source: "light", dest: "temperature" },
+    { source: "temperature", dest: "humidity" },
+    { source: "humidity", dest: "location" },
+  ];
+
+  const seeds = lines[0]
+    .split(": ")[1]
+    .split(" ")
+    .map((seed) => {
+      return parseInt(seed);
+    });
+  console.log(seeds);
+
+  const seeds_ = seeds.reduce((acc, seed, index) => {
+    if (index % 2 === 0) {
+      acc.push({ start: seed, length: seeds[index + 1] });
+    }
+    return acc;
+  }, []);
+  console.log(seeds_);
+  /*
+  let seeds = [];
+  for (let i = 0; i < seeds_.length; i += 2) {
+    seeds.push(
+      Array(seeds_[i + 1])
+        .fill(seeds_[i])
+        .map((seed, index) => {
+          return seed + index;
+        })
+    );
+  }
+  seeds = seeds.flat();
+*/
+
+  const grouped = groupBy(lines, "")
+    .map((group) => {
+      const sourceDest = mapNames.filter((mapName) =>
+        group[0].split("-")[0].includes(mapName.source)
+      )[0];
+      const mappings = group.slice(1).map((line) => {
+        return {
+          out: parseInt(line.split(" ")[0]),
+          in: parseInt(line.split(" ")[1]),
+          length: parseInt(line.split(" ")[2]),
+        };
+      });
+
+      return {
+        source: sourceDest.source,
+        dest: sourceDest.dest,
+        maps: mappings,
+      };
+    })
+    .slice(1);
+
+  let results = [];
+  for (let i = 0; i < seeds.length; i++) {
+    const seed = seeds[i];
+    const result = grouped.reduce((acc, group) => {
+      const selectedMapping = group.maps.filter((mapping) => {
+        return acc >= mapping.in && acc < mapping.in + mapping.length;
+      });
+      selectedMapping.forEach((mapping) => {
+        const diff = acc - mapping.in;
+        if (acc >= mapping.in && acc <= mapping.in + mapping.length) {
+          acc = mapping.out + diff;
+          return acc;
+        }
+      });
+      return acc;
+    }, seed);
+    results.push(result);
+  }
+
+  console.log("Part 2", Math.min(...results));
 };
 
 part1();
