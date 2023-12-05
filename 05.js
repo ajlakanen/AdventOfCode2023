71e323290c3b88f0b3995e414a595154c5b32e06
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-05 18:39:31 +0200

    Day 5 wip

--- 05.js
+++ 05.js
@@ -1,3 +1,4 @@
+const { group } = require("console");
 const f = require("fs");
 
 const part1 = () => {
@@ -122,9 +123,9 @@ const part2 = () => {
     .split(": ")[1]
     .split(" ")
     .map((seed) => {
-      return parseInt(seed);
+      return BigInt(parseInt(seed));
     });
-  console.log(initialSeeds);
+  // console.log(initialSeeds);
 
   const seedRanges = initialSeeds.reduce((acc, seed, index) => {
     if (index % 2 === 0) {
@@ -133,16 +134,18 @@ const part2 = () => {
     return acc;
   }, []);
 
-  const grouped = groupBy(lines, "")
+  // console.log("seedRanges", seedRanges);
+
+  const groupedMaps = groupBy(lines, "")
     .map((group) => {
       const sourceDest = mapNames.filter((mapName) =>
         group[0].split("-")[0].includes(mapName.source)
       )[0];
       const mappings = group.slice(1).map((line) => {
         return {
-          out: parseInt(line.split(" ")[0]),
-          in: parseInt(line.split(" ")[1]),
-          length: parseInt(line.split(" ")[2]),
+          out: BigInt(parseInt(line.split(" ")[0])),
+          in: BigInt(parseInt(line.split(" ")[1])),
+          length: BigInt(parseInt(line.split(" ")[2])),
         };
       });
 
@@ -154,35 +157,71 @@ const part2 = () => {
     })
     .slice(1);
 
-  let results = [];
+  groupedMaps.reverse();
+  // console.log(groupedMaps[0]);
+
+  const whenToStop = groupedMaps[0].maps.reduce(
+    (acc, map) => (map.in > acc ? map.in : acc),
+    groupedMaps[0].maps[0].in
+  );
+  let ii = 0;
+  while (ii < whenToStop) {
+    let minOutput =
+      groupedMaps[0].maps.reduce(
+        (acc, map) => (map.out < acc ? map.out : acc),
+        groupedMaps[0].maps[0].out
+      ) + BigInt(ii);
+
+    //console.log("initial minOutput", minOutput);
+    for (let i = 0; i < groupedMaps.length; i++) {
+      for (let j = 0; j < groupedMaps[i].maps.length; j++) {
+        const min = groupedMaps[i].maps[j].out;
+        const max = groupedMaps[i].maps[j].out + groupedMaps[i].maps[j].length; // exclusive
+        if (min <= minOutput && max > minOutput) {
+          /*console.log(
+            "found",
+            "from",
+            groupedMaps[i].dest,
+            "to",
+            groupedMaps[i].source,
+            min,
+            "<",
+            minOutput,
+            "<",
+            max,
+            " --> ",
+            groupedMaps[i].maps[j].in,
+            "+",
+            minOutput - min
+          );*/
+          const diff = minOutput - min;
+          minOutput = groupedMaps[i].maps[j].in + diff;
+          //console.log("new minOutput", minOutput);
+          break;
+        }
+      }
+    }
+    if (initialSeeds.includes(minOutput)) {
+      console.log("found seed", minOutput);
+      break;
+    } else ii++;
+  }
 
   // TODO: This is too slow :-(. Need to find a way to reduce the number of seeds.
+  /*
+  let mappingLowests = [];
   for (let j = 0; j < seedRanges.length; j++) {
     for (
       let k = seedRanges[j].start;
       k < seedRanges[j].start + seedRanges[j].length;
       k++
     ) {
+      let lowestOfRangeFound = false;
       const seed = k;
-
-      const result = grouped.reduce((acc, group, index) => {
-        const selectedMapping = group.maps.filter((mapping) => {
-          return acc >= mapping.in && acc < mapping.in + mapping.length;
-        });
-        selectedMapping.forEach((mapping) => {
-          const diff = acc - mapping.in;
-          if (acc >= mapping.in && acc <= mapping.in + mapping.length) {
-            acc = mapping.out + diff;
-            return acc;
-          }
-        });
-        return acc;
-      }, seed);
-      results.push(result);
     }
   }
+  */
   /*
-
   let results = [];
   for (let i = 0; i < seeds.length; i++) {
     const seed = seeds[i];
@@ -201,8 +240,8 @@ const part2 = () => {
     }, seed);
     results.push(result);
   }
-*/
-  console.log("Part 2", Math.min(...results));
+  */
+  console.log("Part 2");
 };
 
 part1();
