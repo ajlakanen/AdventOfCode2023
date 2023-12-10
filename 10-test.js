6c80e735b2ab5c28b5337697f2bca80629ad7f6a
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-10 12:57:30 +0200

    Day 1 part 1 wip

--- 10-test.js
+++ 10-test.js
@@ -1,4 +1,9 @@
-import { canConnect, checkConnections, pipes } from "./10.js";
+import {
+  canConnect,
+  getConnectedCardinals,
+  getConnections,
+  pipes,
+} from "./10.js";
 
 const pipe1 = "L";
 const from1 = "N";
@@ -19,7 +24,7 @@ const lines4 =
    "SJLL7", 
    "|F--J", 
    "LJ.LJ"];
-console.log(checkConnections({ y: 4, x: 4 }, lines4).join("") === "0001");
+console.log(getConnections({ y: 4, x: 4 }, lines4).join("") === "0001");
 
 // Bottom row, connections exist
 const lines5 =
@@ -29,7 +34,7 @@ const lines5 =
    "SJLL7", 
    "|F--J", 
    "LJ.LJ"];
-console.log(checkConnections({ y: 4, x: 3 }, lines5).join("") === "0100");
+console.log(getConnections({ y: 4, x: 3 }, lines5).join("") === "0100");
 
 // No connections
 const lines6 =
@@ -39,4 +44,26 @@ const lines6 =
    "SJLL7", 
    "|F-.J", 
    "LJ.L."];
-console.log(checkConnections({ y: 4, x: 3 }, lines6).join("") === "0000");
+console.log(getConnections({ y: 4, x: 3 }, lines6).join("") === "0000");
+
+// Pipe below S, coming from S
+const lines7 =
+  // prettier-ignore
+  ["7-F7-", 
+   ".FJ|7", 
+   "SJLL7", 
+   "|F--J", 
+   "LJ.LJ"];
+console.log(
+  getConnectedCardinals({ y: 3, x: 0 }, "N", lines7).join("") === "S"
+);
+
+const lines8 =
+  // prettier-ignore
+  ["7L7-F7F",
+     "L7L7|||",
+     "7L7S|||",
+     "JFJ|||L"];
+console.log(
+  getConnectedCardinals({ y: 1, x: 3 }, "W", lines8).join("") === "S"
+);
--- 10.js
+++ 10.js
@@ -16,6 +16,7 @@ const pipes = {
   7: ["S", "W"],
   J: ["N", "W"],
   F: ["S", "E"],
+  S: ["N", "E", "S", "W"],
 };
 
 const canConnect = (pipe, from) => {
@@ -23,24 +24,72 @@ const canConnect = (pipe, from) => {
   return pipes[pipe].indexOf(from) > -1;
 };
 
-const checkConnections = ({ y, x }, lines) => {
+const cardinals = {
+  N: [0, -1],
+  E: [1, 0],
+  S: [0, 1],
+  W: [-1, 0],
+};
+
+const cardinalOpposites = {
+  N: "S",
+  E: "W",
+  S: "N",
+  W: "E",
+};
+
+const getConnectedCardinals = ({ y, x }, from, lines) => {
+  let connections = [];
+
+  // north
+  if (from !== "N" && y > 0 && canConnect(lines[y - 1][x], "S"))
+    connections.push("N");
+  // east
+  if (
+    from !== "E" &&
+    x < lines[y].length - 1 &&
+    canConnect(lines[y][x + 1], "W")
+  )
+    connections.push("E");
+  // south
+  if (from !== "S" && y < lines.length - 1 && canConnect(lines[y + 1][x], "N"))
+    connections.push("S");
+  // west
+  if (from !== "W" && x > 0 && canConnect(lines[y][x - 1], "E"))
+    connections.push("W");
+
+  return connections;
+};
+
+const nextLocation = ({ y, x }, lines, from) => {
+  const char = lines[y][x];
+  const newDir = pipes[char].filter((d) => d !== cardinalOpposites[from])[0];
+  //console.log("newDirection", newDirection);
+  const newPos = {
+    newY: y + (newDir === "N" ? -1 : newDir === "S" ? 1 : 0),
+    newX: x + (newDir === "W" ? -1 : newDir === "E" ? 1 : 0),
+    newDir: newDir,
+  };
+
+  console.log("newPos", newPos);
+  return newPos;
+};
+
+const getConnections = ({ y, x }, lines) => {
   let connections = [];
   // north
-  y > 0 && canConnect(lines[y - 1][x], "S")
-    ? connections.push(1)
-    : connections.push(0);
+  if (y > 0 && canConnect(lines[y - 1][x], "S")) connections.push("N");
+
   // east
-  x < lines[y].length - 1 && canConnect(lines[y][x + 1], "W")
-    ? connections.push(1)
-    : connections.push(0);
+  if (x < lines[y].length - 1 && canConnect(lines[y][x + 1], "W"))
+    connections.push("E");
+
   // south
-  y < lines.length - 1 && canConnect(lines[y + 1][x], "N")
-    ? connections.push(1)
-    : connections.push(0);
+  if (y < lines.length - 1 && canConnect(lines[y + 1][x], "N"))
+    connections.push("S");
+
   // west
-  x > 0 && canConnect(lines[y][x - 1], "E")
-    ? connections.push(1)
-    : connections.push(0);
+  if (x > 0 && canConnect(lines[y][x - 1], "E")) connections.push("W");
 
   return connections;
 };
@@ -55,19 +104,37 @@ const part1 = () => {
 
   const startPos = findStartPos(lines);
 
-  const cardinals = {
-    N: 0,
-    E: 1,
-    S: 2,
-    W: 3,
-  };
-
-  const connections = checkConnections(startPos, lines);
-
+  const connections = getConnections(startPos, lines);
+  let prevDir = connections[0];
+  let connectionDirection = cardinalOpposites[prevDir];
+  let y = startPos.x + cardinals[prevDir][0];
+  let x = startPos.y + cardinals[prevDir][1];
+  const firstPipeAfterStart = { y: x, x: y };
   console.log("startPos", startPos);
-  console.log("connections", connections);
+  console.log("startPos connections", connections);
+  console.log("firstConnection", prevDir);
+  console.log("firstConnectionDirection", connectionDirection);
+  console.log("firstPipeAfterStart", firstPipeAfterStart);
+
+  const connections2 = getConnectedCardinals(
+    { y: x, x: y },
+    connectionDirection,
+    lines
+  );
+  console.log("connections2", connections2);
+
+  let i = 1;
+  //let {newY, newX} = firstPipeAfterStart;
+  while (true) {
+    const { newY, newX, newDir } = nextLocation({ y: x, x: y }, lines, prevDir);
+    x = newX;
+    y = newY;
+    prevDir = newDir;
+    if (lines[x][y] === "S") break;
+    else i++;
+  }
 };
 
 part1();
 
-export { canConnect, checkConnections, pipes };
+export { canConnect, getConnections, getConnectedCardinals, pipes };
