3809a0a8b21e88eef2c0c86efaeeb04b845c2b2d
Lakanen Antti-Jussi <antti-jussi.lakanen@jyu.fi>
2023-12-10 10:02:26 +0200

    day 10 wip

--- 10.js
+++ 10.js
@@ -22,6 +22,28 @@ const canConnect = (pipe, from) => {
   return pipes[pipe].indexOf(from) > -1;
 };
 
+const checkConnections2 = ({ y, x }, lines) => {
+  let connections = [];
+  // north
+  y > 0 && canConnect(lines[y - 1][x], "S")
+    ? connections.push(1)
+    : connections.push(0);
+  // east
+  x < lines[y].length && canConnect(lines[y][x + 1], "W")
+    ? connections.push(1)
+    : connections.push(0);
+  // south
+  y < lines.length && canConnect(lines[y + 1][x], "N")
+    ? connections.push(1)
+    : connections.push(0);
+  // west
+  x > 0 && canConnect(lines[y][x - 1], "E")
+    ? connections.push(1)
+    : connections.push(0);
+
+  return connections;
+};
+
 const checkConnections = ([nesw]) => {
   let connections = [];
   // north
@@ -53,7 +75,10 @@ const part1 = () => {
     W: 3,
   };
 
+  const connections = checkConnections2(startPos, lines);
+
   console.log("startPos", startPos);
+  console.log("connections", connections);
 };
 
 part1();
