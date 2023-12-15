af03b8b4e464ff3d10e56c6a23ad429933ffd322
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-15 08:47:34 +0200

    Day 5, try 2

--- 05-2.js
+++ 05-2.js
@@ -1,9 +1,10 @@
 const f = require("fs");
+const { parse } = require("path");
 
 const part2 = () => {
   let lines = [];
 
-  const data = f.readFileSync("data/05-data.txt", "utf-8");
+  const data = f.readFileSync("data/05-data-small.txt", "utf-8");
   data.split(/\r?\n/).forEach((line) => {
     lines.push(line);
   });
@@ -27,27 +28,26 @@ const part2 = () => {
   const seeds = parseSeeds(lines[0]);
   console.log(seeds);
 
-  let mapGroups = [];
-  let currentMaps = [];
-  // Maps start at line 2
-  for (let i = 2; i < lines.length; i++) {
-    const line = lines[i];
-    if (line === "") {
-      mapGroups.push(currentMaps);
-      currentMaps = [];
-      continue;
-    }
-    if (line.includes(":")) continue;
-    const [output, input, length] = line.split(" ").map((l) => parseInt(l));
-    currentMaps.push({ output, input, length });
-  }
-  //console.log(mapGroups);
+  const parseMapGroups = (lines) => {
+    let mapGroups = [];
+    let currentMaps = [];
 
-  // for each seedRange
-  //   for each mapGroup
-  //     for each map
-  //       if map.input is in seedRange
-  //         apply map to seedRange
+    // Maps start at line 2
+    for (let i = 2; i < lines.length; i++) {
+      const line = lines[i];
+      if (line === "") {
+        mapGroups.push(currentMaps);
+        currentMaps = [];
+        continue;
+      }
+      if (line.includes(":")) continue;
+      const [output, input, length] = line.split(" ").map((l) => parseInt(l));
+      currentMaps.push({ output, input, length });
+    }
+    return mapGroups;
+  };
+
+  const mapGroups = parseMapGroups(lines);
 
   // [<--MAP-->]
   //             [<--VALUES-->]
@@ -184,28 +184,37 @@ const part2 = () => {
     }
   };
 
-  for (let i = 0; i < seeds.length; i++) {
-    const seedRange = seeds[i];
-
+  const mapRangeMin = (values, mapGroups) => {
+    // We have our value range, values.
+    // We want to find the range of values that is the smallest
+    // when mapped through the mapGroups.
+    let rangeMin;
     for (let j = 0; j < mapGroups.length; j++) {
-      const mapGroup = mapGroups[j];
+      const mapGroup = mapGroups[j].sort((a, b) => a.input - b.input);
 
       let k = 0;
+
+      // This array will contain the mapped and unmapped values
+      let result;
       while (k < mapGroup.length) {
         const map = mapGroup[k];
-        const result = applyMap(seedRange, map);
-        console.log(result);
-        //return;
-        // apply map to seedRange if necessary
-        // const newSeed = {
-        //   start: map.output,
-        //   length: map.length,
-        // };
-        // seeds.push(newSeed);
-        k++;
+        result = applyMap(values, map);
+        //console.log("result.unmapped", result.unmapped);
+
+        // All values have been mapped, we can move on to the next mapGroup
+        if (result.unmapped === undefined || result.unmapped.length === 0) {
+          break;
+        } else {
+          k++;
+        }
+      }
+      console.log("result", result, "j", j);
+      if ((j === result.mapped) !== undefined) {
       }
     }
-  }
+    return rangeMin;
+  };
+  console.log(mapRangeMin(seeds[0], mapGroups));
 };
 
 part2();
