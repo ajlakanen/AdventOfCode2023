2d9f39494f0c3ad12a38107cbb93e9f6077e092a
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2023-12-15 23:22:04 +0200

    Day 15 part 2 wip

--- 15.js
+++ 15.js
@@ -2,7 +2,7 @@ const f = require("fs");
 
 const smallData = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 const bigData = f.readFileSync("data/15-data.txt", "utf-8");
-const data = bigData.split(",");
+const data = smallData.split(",");
 
 const hash = (str) => {
   let hash = 0;
@@ -16,10 +16,38 @@ const hash = (str) => {
 };
 
 const part1 = () => {
-  console.log(hash("HASH"));
-  console.log(hash("rn=1"));
   const sum = data.reduce((acc, now) => acc + hash(now), 0);
   console.log(sum);
 };
 
+const part2 = () => {
+  let boxes = Array(256).fill("");
+  for (let i = 0; i < data.length; i++) {
+    const equal = data[i].indexOf("=");
+    const dash = data[i].indexOf("-");
+    const name = data[i].substring(0, Math.max(equal, dash));
+    const box = hash(name);
+    if (dash > -1) {
+      boxes[box] = [...boxes[box]].filter(
+        (item) => item.split("=")[0] !== name
+      );
+    } else {
+      if (
+        [...boxes[box]].filter((item) => item.split("=")[0] === name).length > 0
+      ) {
+        const toReplace = [...boxes[box]].filter(
+          (item) => item.split("=")[0] === name
+        )[0];
+        const indexToReplace = boxes[box].indexOf(toReplace);
+        console.log(boxes[box], toReplace, indexToReplace);
+        boxes[box][indexToReplace] = data[i];
+        continue;
+      }
+      boxes[box] = [...boxes[box], data[i]];
+    }
+  }
+  console.log(boxes);
+};
+
 part1();
+part2();
