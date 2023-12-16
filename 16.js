7ec923a258870c5e6169c62834fae8182f77d56f
Lakanen Antti-Jussi <antti-jussi.lakanen@jyu.fi>
2023-12-16 10:35:09 +0200

    Day 16 wip

--- 16.js
+++ 16.js
@@ -10,7 +10,7 @@ const lines = f
 const energized = (map, { posX, posY }, { dirX, dirY }, energizedPositions) => {
   const { newX, newY } = { newx: posX + dirX, newY: posY + dirY };
   if (newX < 0 || newX >= map[0].length || newY < 0 || newY >= map.length) {
-    return { isInMap: false, amount: amount };
+    return { isInMap: false, energizedPositions: energizedPositions };
   }
 
   const newChar = map[newY][newX];
@@ -20,11 +20,13 @@ const energized = (map, { posX, posY }, { dirX, dirY }, energizedPositions) => {
         (item) => item.posX === newX && item.posY === newY
       ).length === 0
     ) {
-      energizedPositions.push({ posX: newX, posY: newY });
+      energizedPositions = [
+        ...energizedPositions.push({ posX: newX, posY: newY }),
+      ];
     }
     return {
       isInMap: true,
-      amount: energized(
+      energizedPositions: energized(
         map,
         { posX: newX, posY: newY },
         { dirX, dirY },
@@ -36,10 +38,20 @@ const energized = (map, { posX, posY }, { dirX, dirY }, energizedPositions) => {
   } else if (newChar === "L") {
     return { isInMap: true, amount: 0 };
   }
+  return energizedPositions;
 };
 
 const part1 = () => {
   lines.forEach((line) => console.log(line));
+  const energizedPositions = [];
+  console.log(
+    energized(
+      lines,
+      { posX: 0, posY: 0 },
+      { dirX: 1, dirY: 0 },
+      energizedPositions
+    )
+  );
 };
 
 part1();
